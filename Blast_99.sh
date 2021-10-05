09bd027f5d1a33ebea8399efc1a7d2388384cf73
liluacrobat <lilu.acrobat@gmail.com>
2021-10-05 17:22:39 -0400

    Update Blast_99.sh

--- Blast_99.sh
+++ Blast_99.sh
@@ -18,7 +18,7 @@ echo 'preprocessing...'
 START=`date +%s`
 
 
-/projects/academic/pidiazmo/Pipeline/blast+/2.6.0/bin/makeblastdb -in /projects/academic/pidiazmo/vinita/ref_db/HOMD_16S_rRNA_RefSeq_V15.22.fasta -out HOMD -dbtype 'nucl' -input_type fasta
+/projects/academic/pidiazmo/Pipeline/blast+/2.6.0/bin/makeblastdb -in /projects/academic/pidiazmo/16S_Database/HOMD/eHOMD15.2/HOMD_16S_rRNA_RefSeq_V15.22.fasta -out HOMD -dbtype 'nucl' -input_type fasta
 
 /projects/academic/pidiazmo/Pipeline/blast+/2.6.0/bin/blastn -query ./dna-sequences.fasta -task megablast -db HOMD  -perc_identity 99 -qcov_hsp_perc 99 -max_target_seqs 5000 -outfmt "7 qacc sacc qstart qend sstart send length pident qcovhsp qcovs" -out blast_99_taxonomy
 
